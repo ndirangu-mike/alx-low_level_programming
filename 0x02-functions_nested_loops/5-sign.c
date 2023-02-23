94ffd9acb5e56dcc09c222902a2ee5cf5e3faf9e
root <ndirangu.mikengari@gmail.com>
2023-02-23 00:42:40 -0800

    created a 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: number being evaluated
+ *
+ * Description: evaluates the sign of a number
+ * Return: 0 if n is zero, -1 and prints - if < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
