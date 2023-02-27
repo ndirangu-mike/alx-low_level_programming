3a18357792c4930ecd1a6ce4d10056e0fd3f2a42
root <ndirangu.mikengari@gmail.com>
2023-02-27 12:39:01 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+include "main.h"
+/* fuction to test whether a number is positive or negative
+ * Return 0
+ */ 
+
+int main(void);
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0)                                                        {	
+
---
+++ 0x04-more_functions_nested_loops/0-issuper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * 0-isupper.c - check the uppercase.
+ *
+ * Return: 0
+ */ 
+int main(void)
+{
+    char c;
+
+    c = 'A'
+    printf("%c: %d/n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (1)
+}	    
