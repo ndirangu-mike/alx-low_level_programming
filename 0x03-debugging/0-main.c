2bd1734157bd110ebdc0a88a8b65cb17d94503f2
root <ndirangu.mikengari@gmail.com>
2023-02-27 23:34:14 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 include "main.h"
-/* fuction to test whether a number is positive or negative
- * Return 0
- */ 
+/**
+* test function positive or negative
+* Return: 0
+*/ 
 
-int main(void);
+int main(void) 
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative
 
-	return (0)                                                        {	
-
+return (0);
+}
