52bcfe38d9de41887014fe7396e8a73dc4848ca0
root <ndirangu.mikengari@gmail.com>
2023-02-27 23:49:28 -0800

    created a 0-main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 include "main.h"
 /**
-* test function positive or negative
-* Return: 0
-*/ 
+ * test function positive or negative
+ * Return: 0
+ */
 
-int main(void) 
+int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative
+	i = 0; 
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
